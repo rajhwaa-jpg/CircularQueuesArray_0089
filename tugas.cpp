6d96110dddfc9937b9e67685e8243e1bbd770e8c
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-28 22:16:26 +0700

    add display method to Queues class for queue state visualization

--- tugas.cpp
+++ tugas.cpp
@@ -70,4 +70,12 @@ public:
             FRONT = FRONT + 1;
         }
     }
+
+    void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        
+    }
 };
