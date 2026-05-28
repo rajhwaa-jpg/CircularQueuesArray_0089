99b214be539c007f263ea1fff4558fa3747fb6be
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-28 22:15:04 +0700

    jika elemen yang di hapus  berada di posisi terakhir array, kembali k awal array

--- tugas.cpp
+++ tugas.cpp
@@ -62,6 +62,12 @@ public:
             FRONT = -1;
             REAR = -1;
         }
-        
+        else{
+            //jika elemen yang di hapus  berada di posisi terakhir array, kembali k awal array
+            if (FRONT == max - 1)
+            FRONT = 0;
+            else
+            FRONT = FRONT + 1;
+        }
     }
 };
