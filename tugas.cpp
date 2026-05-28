3ec44bc0dbaa113e09aaa6f6ffff2c570dca2d85
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-28 22:32:38 +0700

    add queue elements display logic to display method in Queues class

--- tugas.cpp
+++ tugas.cpp
@@ -82,6 +82,17 @@ public:
             cout << "queue is empty\n";
             return;
         }
-        
+        cout << "\nelements in te queue are....\n";
+
+        //jika FRONT <= REAR, iterasi dri FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
