f5ee17c7f347214e810a3758f3cd2c0f78da0c34
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-28 22:40:57 +0700

    add main function with menu for queue operations

--- tugas.cpp
+++ tugas.cpp
@@ -113,3 +113,57 @@ public:
         }
     }
 };
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "menu" << endl;
+            cout << "1. implement insert operation" << endl;
+            cout << "2. implement delete operation" << endl;
+            cout << "3. display values" << endl;
+            cout << "4. exit" << endl;
+            cout << "enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                exit(0);
+            }
+            default:
+            {
+                cout << "Invalid option" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
+    }
+    return 0;
+}
